3c2f4d7e8d596a1e79536f0cf6e4e491402daa37
David Boedigheimer <stealth259@gmail.com>
2020-07-09 15:54:17 -0700

    Add list and start of for loop.

--- PingPongC.cs
+++ PingPongC.cs
@@ -4,8 +4,15 @@ class PingPong
 {
     static void Main()
     {
+        List<int> pingPongList = new List<int>();
+
         Console.WriteLine("Enter a number!");
         string stringNumber = Console.ReadLine();
         int userNumber = int.Parse(stringNumber);
+
+        for (int i = 0; i < pingPongList.length; i++)
+        {
+            
+        }
     }
 }
